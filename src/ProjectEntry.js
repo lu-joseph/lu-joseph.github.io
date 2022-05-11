ca22041d83f426eeb93e5910f06f23a9ebc6a731
lu-joseph <josephlu414@gmail.com>
2022-05-11 00:41:37 -0400

    Response Web Design implemented

--- src/ProjectEntry.js
+++ src/ProjectEntry.js
@@ -3,17 +3,19 @@ import "./css/ProjectEntry.css";
 
 const ProjectEntry = (props) => {
   return (
-    <div class="entry-container">
-      <div class="image-container">
-        <img class="image" src={props.imgsrc} alt="Project"></img>
-      </div>
-      <div class="text-container">
-        <div class="title">{props.title}</div>
-        <ul class="details">
-          {props.details.map((item) => (
-            <ProjectEntryDetail key={item.id} text={item.text} />
-          ))}
-        </ul>
+    <div class="entry-card">
+      <div class="title">{props.title}</div>
+      <div class="entry-container row">
+        <div class="image-container col-4">
+          <img class="image" src={props.imgsrc} alt="Project"></img>
+        </div>
+        <div class="text-container col-8">
+          <ul class="details">
+            {props.details.map((item) => (
+              <ProjectEntryDetail key={item.id} text={item.text} />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
