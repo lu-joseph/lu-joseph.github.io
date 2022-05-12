92f964f0a1b0e2db7c632102197649e680be60b6
lu-joseph <josephlu414@gmail.com>
2022-05-11 23:28:07 -0400

    Initial contact page work

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,42 @@
 const Contact = () => {
-  return <h1>Contact</h1>;
+  return (
+    <div className="contact-entry">
+      <div className="entry-card">
+        <div className="title-container">
+          <div className="title">Contact Me!</div>
+        </div>
+        <div className="details-container">
+          <div className="details">
+            <div className="description">
+              I'm currently looking for internships in software development,
+              preferably backend, for fall 2022.
+            </div>
+            <div className="links">
+              Links:
+              <ul>
+                <li>
+                  <a
+                    href="https://github.com/lu-joseph"
+                    className="github-link"
+                  >
+                    My Github
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/lu-joseph/"
+                    className="linkedin-link"
+                  >
+                    My LinkedIn
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
