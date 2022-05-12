76e831f72e08a07169df6f12d4a1f51e647bb643
lu-joseph <josephlu414@gmail.com>
2022-05-12 17:31:42 -0400

    Contact page progress

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,5 @@
+import "../css/Contact.css";
+
 const Contact = () => {
   return (
     <div className="contact-entry">
@@ -8,29 +10,29 @@ const Contact = () => {
         <div className="details-container">
           <div className="details">
             <div className="description">
-              I'm currently looking for internships in software development,
-              preferably backend, for fall 2022.
+              I'm currently looking for fall 2022 internships in software
+              development. I prefer backend work, but I am open to fullstack and
+              frontend work as well. Check the{" "}
+              <a href="https://josephlu.ca/about" className="about-page-link">
+                About
+              </a>{" "}
+              page to see my skills in detail!
             </div>
             <div className="links">
-              Links:
-              <ul>
-                <li>
-                  <a
-                    href="https://github.com/lu-joseph"
-                    className="github-link"
-                  >
-                    My Github
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.linkedin.com/in/lu-joseph/"
-                    className="linkedin-link"
-                  >
-                    My LinkedIn
-                  </a>
-                </li>
-              </ul>
+              <a href="https://github.com/lu-joseph" className="github-link">
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/lu-joseph/"
+                className="linkedin-link"
+              >
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://www.facebook.com/profile.php?id=100009574503377">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <span className="email">joseph.lu1@uwaterloo.ca</span>
+              <span className="phone-number">(226)-789-1686</span>
             </div>
           </div>
         </div>
