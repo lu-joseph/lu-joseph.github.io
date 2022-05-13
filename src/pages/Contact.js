56c6545cdb010d200a600a8bd64c377f166456fe
lu-joseph <josephlu414@gmail.com>
2022-05-13 09:42:33 -0400

    Contact page updated and build run

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,29 +10,38 @@ const Contact = () => {
         <div className="details-container">
           <div className="details">
             <div className="description">
-              I'm currently looking for fall 2022 internships in software
-              development. I prefer backend work, but I am open to fullstack and
-              frontend work as well. Check the{" "}
-              <a href="https://josephlu.ca/about" className="about-page-link">
-                About
-              </a>{" "}
+              I'm currently looking for Fall 2022 internships in Software
+              Development. I specialize in <b>Backend</b> work, but I am
+              developing my <b>Fullstack</b> and <b>Frontend</b> skills as well.
+            </div>
+            <div className="description second-paragraph">
+              Check the{" "}
+              <b>
+                <a href="https://josephlu.ca/about" className="about-page-link">
+                  About
+                </a>{" "}
+              </b>
               page to see my skills in detail!
             </div>
-            <div className="links">
-              <a href="https://github.com/lu-joseph" className="github-link">
-                <i className="fa fa-github"></i>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/lu-joseph/"
-                className="linkedin-link"
-              >
-                <i className="fa fa-linkedin-square"></i>
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=100009574503377">
-                <i className="fa fa-facebook-square"></i>
-              </a>
-              <span className="email">joseph.lu1@uwaterloo.ca</span>
-              <span className="phone-number">(226)-789-1686</span>
+            <div className="contact-info">
+              <div className="icons">
+                <a href="https://github.com/lu-joseph" className="github-link">
+                  <i className="fa fa-github"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/lu-joseph/"
+                  className="linkedin-link"
+                >
+                  <i className="fa fa-linkedin-square"></i>
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=100009574503377">
+                  <i className="fa fa-facebook-square"></i>
+                </a>
+              </div>
+              <div className="email-phone">
+                <span className="email">joseph.lu1@uwaterloo.ca</span>
+                <span className="phone-number">(226)-789-1686</span>
+              </div>
             </div>
           </div>
         </div>
