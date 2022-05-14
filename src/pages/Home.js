bde6ffe70ef30eda76b5b7238e39fbc895c3fd5d
lu-joseph <josephlu414@gmail.com>
2022-05-14 13:55:12 -0400

    Finished home page and build run

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,31 +8,31 @@ const Home = () => {
           <div className="title">Welcome to my site!</div>
         </div>
         <div className="details-container">
-          <div className="description">
-            <div className="details">
-              My name is Joseph Lu, and I'm a computer science student at the
-              University of Waterloo.
-            </div>
-            <div className="details">
-              I'm currently looking for Fall 2022 internships in Software
-              Development.
-            </div>
-            <div className="details">
-              I specialize in <b>Backend</b> work, but I am developing my{" "}
-              <b>Fullstack</b> and <b>Frontend</b> skills as well.
-            </div>
-            <div className="details">
-              Check the{" "}
-              <b>
-                <a href="https://josephlu.ca/about" className="about-page-link">
-                  About
-                </a>{" "}
-              </b>
-              page to see my skills in detail!
-            </div>
+          <img
+            className="home-page-image"
+            src="./images/home-me-2.jpg"
+            alt=""
+          ></img>
+          <div className="details">
+            My name is Joseph Lu, and I'm a computer science student at the
+            University of Waterloo.
           </div>
-          <div className="image-container">
-            <img src="./images/home-me-2.jpg" alt=""></img>
+          <div className="details">
+            I'm currently looking for Fall 2022 internships in Software
+            Development.
+          </div>
+          <div className="details">
+            I specialize in <b>Backend</b> work, but I am developing my{" "}
+            <b>Fullstack</b> and <b>Frontend</b> skills as well.
+          </div>
+          <div className="details">
+            Check the{" "}
+            <b>
+              <a href="https://josephlu.ca/about" className="about-page-link">
+                About
+              </a>{" "}
+            </b>
+            page to see my skills in detail!
           </div>
         </div>
       </div>
